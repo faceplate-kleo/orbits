605f545208a1c765e92d881d4c9c3fa84d75c56d
Kleo <faceplatemusic@gmail.com>
2023-03-08 10:14:47 -0700

    started implementing transfer orbit calculations and a couple tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,35 @@ pub mod orbmech;
 
 extern crate vecmath;
 
+
+#[cfg(test)]
+mod tests {
+    use crate::orbmech;
+    fn is_within_percentage(a: f64, b: f64, margin: f64) -> bool {
+        (a - b) / b < margin
+    }
+    #[test]
+    fn test_dv_calculation() {
+        let earth_orbital_speed: f64 = 29.7827; //km/s 
+
+        let mars_orbital_speed: f64 = 24.07; //km/s  
+        
+
+        let earth_mars_theta: f64 = 44.0;
+
+        let earth_mars_dv = orbmech::calculate_transfer_dv(earth_orbital_speed, mars_orbital_speed, earth_mars_theta);
+
+
+        println!("{}", earth_mars_dv);
+        assert!(is_within_percentage(earth_mars_dv, 4.3, 0.2)) 
+    }
+    #[test]
+    fn test_vecangle() {
+        let veca: vecmath::Vector3<f64> =  [  1.0,  0.0,  0.0 ];
+        let vecb: vecmath::Vector3<f64> =  [  0.0,  1.0,  0.0 ];
+
+        let theta = orbmech::angle_between(&veca, &vecb) * 57.2958;
+
+        assert!(is_within_percentage(theta, 90.0, 0.1));
+    }
+}
--- src/orbmech.rs
+++ src/orbmech.rs
@@ -7,18 +7,30 @@ pub struct PlanetSystem {
 }
 
 
-pub struct ConicSection {
-    pub apoapsis: f64,
-    pub periapsis: f64,
+pub struct KeplerOrbit {
     pub eccentricity: f64,
-    pub inclination: f64
+    pub semimajor_axis: f64,
+    pub inclination: f64,
+    pub longitude_an: f64,
+    pub arg_periapsis: f64,
+    pub true_anomaly: f64
+
 }
+
 pub fn au_to_km(dist_au: &f64) -> f64 {
     return dist_au * 149597870.700_f64;
 }
 
 pub fn km_to_mi(dist_km: &f64) -> f64 {
     return dist_km * 0.621371_f64; 
+}
+
+pub fn angle_between(a: &vecmath::Vector3<f64>, b: &vecmath::Vector3<f64>) -> f64 {
+    let dot = vecmath::vec3_dot(*a, *b);
+    let mag_a = vecmath::vec3_len(*a);
+    let mag_b = vecmath::vec3_len(*b);
+
+    (dot / (mag_a * mag_b)).acos()
 }
 
 pub fn calculate_soi_radius(semi_maj_axis: &f64, mass_a: &f64, mass_b: &f64) -> f64 { 
@@ -37,3 +49,22 @@ pub fn generate_soi_hm(solar_system: &HashMap<&'static str, PlanetSystem>, stell
 
     return soi_hm;
 }
+
+
+
+pub fn calculate_transfer_dv_vectors(v_transfer: vecmath::Vector3<f64>, v_parking: vecmath::Vector3<f64>, transfer_angle: f64) -> f64{
+    //v_transfer (->Vc) is the circular velocity of object 1 with respect to parent object
+    //v_parking (->V) is the orbit velocity
+    //transfer_angle (theta) is the flight path angle of v_parking
+
+    let mag_vt = vecmath::vec3_len(v_transfer);
+    let mag_vp = vecmath::vec3_len(v_parking);
+    calculate_transfer_dv(mag_vt, mag_vp, transfer_angle)
+}
+
+pub fn calculate_transfer_dv(mag_vt: f64, mag_vp: f64, transfer_angle: f64) -> f64 {
+    
+    ((mag_vt * mag_vt) + (mag_vp * mag_vp) - (2.0 * mag_vt * mag_vp * transfer_angle.cos())).sqrt()
+}
+
+
